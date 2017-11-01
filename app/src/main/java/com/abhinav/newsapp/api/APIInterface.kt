d17b380219934c8b7aea811f29957d86628e37e9
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-01 16:45:24 +0530

    API interface for retrofit

---
+++ app/src/main/java/com/abhinav/newsapp/api/APIInterface.kt
@@ -0,0 +1,38 @@
+package com.abhinav.newsapp.ui.api
+
+import com.abhinav.newsapp.ui.model.ArticlesResponse
+import com.abhinav.newsapp.ui.model.SourceResponse
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+/**
+ * Created by abhinav.sharma on 31/10/17.
+ */
+interface APIInterface {
+
+    companion object {
+        val NEWSAPI_URL = "https://newsapi.org/v1/"
+
+        fun getNewsAPIService(): APIInterface {
+            return Retrofit.Builder()
+                    .baseUrl(NEWSAPI_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(APIInterface::class.java)
+        }
+    }
+
+    @GET("sources")
+    fun getSources(@Query("language") language: String?,
+                   @Query("category") category: String?,
+                   @Query("country") country: String?): Call<SourceResponse>
+
+    @GET("articles")
+    fun getArticles(@Query("source") source: String,
+                    @Query("sortBy") sortBy: String?,
+                    @Query("apiKey") apiKey: String): Call<ArticlesResponse>
+}
