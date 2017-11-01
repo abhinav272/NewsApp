75164bb295aff4d163ecf0f39dc7fb005208c245
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-01 16:41:49 +0530

    code to add NewsSourceFragment

--- app/src/main/java/com/abhinav/newsapp/ui/HomeActivity.kt
+++ app/src/main/java/com/abhinav/newsapp/ui/HomeActivity.kt
@@ -1,16 +1,20 @@
 package com.abhinav.newsapp.ui
 
-import android.arch.lifecycle.LifecycleActivity
 import android.os.Bundle
 import com.abhinav.newsapp.R
 
 /**
  * Created by abhinav.sharma on 31/10/17.
  */
-class HomeActivity : LifecycleActivity() {
+class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        showSourceFragment()
+    }
+
+    fun showSourceFragment() {
+        addFragment(NewsFragment(), R.id.container, "NewsFragment")
     }
 }
