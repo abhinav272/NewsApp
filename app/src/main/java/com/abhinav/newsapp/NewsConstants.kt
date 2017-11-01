a200814e982d996cb6559ba8f124dcf1779b4320
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-01 16:47:07 +0530

    constants file created

---
+++ app/src/main/java/com/abhinav/newsapp/NewsConstants.kt
@@ -0,0 +1,10 @@
+package com.abhinav.newsapp.ui
+
+/**
+ * Created by abhinav.sharma on 31/10/17.
+ */
+
+object NewsConstants {
+    const val T_SOURCE = "t_source"
+    const val T_ARTICLE = "t_article"
+}
