70e25fde21e0f82710f96dee74b0bd80633cbe8e
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-02 21:57:24 +0530

    NewsSource view holder updated

--- app/src/main/java/com/abhinav/newsapp/adapter/viewholder/NewsSourceViewHolder.kt
+++ app/src/main/java/com/abhinav/newsapp/adapter/viewholder/NewsSourceViewHolder.kt
@@ -2,15 +2,18 @@ package com.abhinav.newsapp.adapter.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.TextView
+import com.abhinav.newsapp.ui.model.Source
 import kotlinx.android.synthetic.main.layout_news_source_single.view.*
 
 /**
  * Created by abhinav.sharma on 01/11/17.
  */
-class NewsSourceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class NewsSourceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    val tvSourceName = itemView.tv_source_name
-    val tvSourceCategory = itemView.tv_category
-    val tvSourceDescription = itemView.tv_source_description
+    fun bind(source: Source, listener: (Source) -> Unit) = with(itemView) {
+        tv_source_name.text = source.name
+        tv_source_description.text = source.description
+        tv_category.text = source.category
+        itemView.setOnClickListener { listener(source) }
+    }
 }
