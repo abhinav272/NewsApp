fc24f0b049fc6331816caf551387723d7f39a4aa
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-02 21:56:04 +0530

    NewsFragment updated for showing news article

--- app/src/main/java/com/abhinav/newsapp/ui/NewsFragment.kt
+++ app/src/main/java/com/abhinav/newsapp/ui/NewsFragment.kt
@@ -5,26 +5,29 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.abhinav.newsapp.R
+import com.abhinav.newsapp.adapter.NewsArticleAdapter
 import com.abhinav.newsapp.adapter.NewsSourceAdapter
+import com.abhinav.newsapp.ui.model.ArticlesResponse
 import com.abhinav.newsapp.ui.model.Source
 import com.abhinav.newsapp.ui.model.SourceResponse
 import com.abhinav.newsapp.ui.viewmodel.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_news.*
 
 /**
  * Created by abhinav.sharma on 01/11/17.
  */
-class NewsFragment : LifecycleFragment() {
+class NewsFragment : LifecycleFragment(), (Source) -> Unit {
 
-    private var recyclerView: RecyclerView? = null
-    private var newsViewModel: NewsViewModel? = null
-    private var observerNewsSource: Observer<SourceResponse>? = null
-    private var newsSourceAdapter: NewsSourceAdapter? = null
-    private var sourceList  = ArrayList<Source>()
+    private lateinit var newsViewModel: NewsViewModel
+    private lateinit var observerNewsSource: Observer<SourceResponse>
+    private lateinit var observerNewsArticle: Observer<ArticlesResponse>
+    private lateinit var newsSourceAdapter: NewsSourceAdapter
+    private lateinit var newsArticleAdapter: NewsArticleAdapter
+    private val sourceList  = ArrayList<Source>()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View? = inflater?.inflate(R.layout.fragment_news, container, false)
@@ -34,19 +37,33 @@ class NewsFragment : LifecycleFragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView = view?.findViewById(R.id.recyclerView)
-        newsSourceAdapter = NewsSourceAdapter(sourceList)
-        recyclerView?.adapter = newsSourceAdapter
-        recyclerView?.layoutManager = LinearLayoutManager(activity)
+        newsSourceAdapter = NewsSourceAdapter(this, sourceList)
+        recyclerView.adapter = newsSourceAdapter
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
         observerNewsSource = Observer { newsSource ->
             sourceList.clear()
             if (newsSource?.sources != null) {
                 sourceList.addAll(newsSource.sources)
-                newsSourceAdapter!!.notifyDataSetChanged()
+                newsSourceAdapter.notifyDataSetChanged()
             }
 
         }
-        newsViewModel?.getNewsSource(null, null, null)
-                ?.observe(this, observerNewsSource)
+
+        observerNewsArticle = Observer { newsArticle ->
+            if (newsArticle != null) {
+                newsArticleAdapter = NewsArticleAdapter(newsArticle.articles!!)
+                recyclerView.adapter = newsArticleAdapter
+            }
+        }
+
+
+        newsViewModel.getNewsSource(null, null, null)
+                .observe(this, observerNewsSource)
+    }
+
+    override fun invoke(source: Source) {
+        newsViewModel.getNewsArticles(source.id!!, source.sortBysAvailable?.get(0))
+                .observe(this, observerNewsArticle)
     }
 }
