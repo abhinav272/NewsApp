8857dff505f0ee4db9b3ea217906edbe954bb6e1
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-02 21:57:39 +0530

    news source click listner added

--- app/src/main/java/com/abhinav/newsapp/adapter/NewsSourceAdapter.kt
+++ app/src/main/java/com/abhinav/newsapp/adapter/NewsSourceAdapter.kt
@@ -1,36 +1,29 @@
 package com.abhinav.newsapp.adapter
 
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.abhinav.newsapp.R
 import com.abhinav.newsapp.adapter.viewholder.NewsSourceViewHolder
+import com.abhinav.newsapp.inflate
 import com.abhinav.newsapp.ui.model.Source
 
 /**
  * Created by abhinav.sharma on 01/11/17.
  */
-class NewsSourceAdapter(private val sourceList : List<Source>) : RecyclerView.Adapter<NewsSourceViewHolder>() {
+class NewsSourceAdapter(private val listener: (Source) -> Unit, private val sourceList: List<Source>) : RecyclerView.Adapter<NewsSourceViewHolder>() {
 
 
     override fun getItemCount(): Int {
         return sourceList.size
     }
 
-    private fun getItem(position: Int) : Source{
+    private fun getItem(position: Int): Source {
         return sourceList[position]
     }
 
-    override fun onBindViewHolder(holder: NewsSourceViewHolder, position: Int) {
-        val source = getItem(position)
-        holder.tvSourceName.text = source.name
-        holder.tvSourceCategory.text = source.category
-        holder.tvSourceDescription.text = source.description
-    }
+    override fun onBindViewHolder(holder: NewsSourceViewHolder, position: Int) =
+            holder.bind(getItem(position), listener)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsSourceViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val itemView = layoutInflater.inflate(R.layout.layout_news_source_single, parent, false)
-        return NewsSourceViewHolder(itemView)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsSourceViewHolder =
+            NewsSourceViewHolder(parent.inflate(R.layout.layout_news_source_single))
 }
