dbc21ad54a161e30fc8d07f8bbbe8d0ab8d161bd
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-04 17:13:39 +0530

    ArticleEntity created

---
+++ app/src/main/java/com/abhinav/newsapp/db/ArticleEntity.kt
@@ -0,0 +1,22 @@
+package com.abhinav.newsapp.db
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import com.abhinav.newsapp.ui.NewsConstants
+
+/**
+ * Created by abhinav.sharma on 04/11/17.
+ */
+
+@Entity(tableName = NewsConstants.T_ARTICLE)
+data class ArticleEntity(
+        @PrimaryKey(autoGenerate = true)
+        val id: Int,
+        val source: String?,
+        val author: String?,
+        val title: String?,
+        val description: String?,
+        val url: String?,
+        val urlToImage: String?,
+        val publishedAt: String?
+)
