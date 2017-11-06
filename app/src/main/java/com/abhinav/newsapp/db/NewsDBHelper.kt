e5d29b04d13ee5f5c00fcf864ded026510406bed
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-06 13:32:19 +0530

    DB helper created

---
+++ app/src/main/java/com/abhinav/newsapp/db/NewsDBHelper.kt
@@ -0,0 +1,28 @@
+package com.abhinav.newsapp.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.abhinav.newsapp.ui.NewsConstants
+
+/**
+ * Created by abhinav.sharma on 04/11/17.
+ */
+@Database(entities = arrayOf(SourceDao::class, ArticleDao::class), version = 1)
+abstract class NewsDBHelper : RoomDatabase() {
+    abstract fun getArticleDao(): ArticleDao
+    abstract fun getSourceDao(): SourceDao
+
+    companion object {
+        private var db: NewsDBHelper? = null
+
+        fun getInstance(context: Context): NewsDBHelper {
+            if (db == null) {
+                db = Room.databaseBuilder(context, NewsDBHelper::class.java, NewsConstants.DB_NAME).build()
+            }
+            return db!!
+        }
+    }
+
+}
