4802fd2d3ade3a8cb2b3ed4c6d25ec77b5eb7b0b
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-07 23:08:26 +0530

    input parameter type changed

--- app/src/main/java/com/abhinav/newsapp/adapter/NewsSourceAdapter.kt
+++ app/src/main/java/com/abhinav/newsapp/adapter/NewsSourceAdapter.kt
@@ -4,20 +4,21 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.abhinav.newsapp.R
 import com.abhinav.newsapp.adapter.viewholder.NewsSourceViewHolder
+import com.abhinav.newsapp.db.SourceEntity
 import com.abhinav.newsapp.inflate
 import com.abhinav.newsapp.ui.model.Source
 
 /**
  * Created by abhinav.sharma on 01/11/17.
  */
-class NewsSourceAdapter(private val listener: (Source) -> Unit, private val sourceList: List<Source>) : RecyclerView.Adapter<NewsSourceViewHolder>() {
+class NewsSourceAdapter(private val listener: (SourceEntity) -> Unit, private val sourceList: List<SourceEntity>) : RecyclerView.Adapter<NewsSourceViewHolder>() {
 
 
     override fun getItemCount(): Int {
         return sourceList.size
     }
 
-    private fun getItem(position: Int): Source {
+    private fun getItem(position: Int): SourceEntity {
         return sourceList[position]
     }
 
