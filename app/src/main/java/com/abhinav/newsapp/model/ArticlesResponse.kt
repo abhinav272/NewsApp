96279fcb532e9a246103cae7b516aca0f58bd84b
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-07 23:13:00 +0530

    items initialised for Response class

--- app/src/main/java/com/abhinav/newsapp/model/ArticlesResponse.kt
+++ app/src/main/java/com/abhinav/newsapp/model/ArticlesResponse.kt
@@ -8,17 +8,17 @@ import com.google.gson.annotations.SerializedName
 
 
 data class ArticlesResponse(
-		@SerializedName("status") val status: String?, //ok
-		@SerializedName("source") val source: String?, //abc-news-au
-		@SerializedName("sortBy") val sortBy: String?, //top
-		@SerializedName("articles") val articles: List<Article>?
+		@SerializedName("status") var status: String?, //ok
+		@SerializedName("source") var source: String?, //abc-news-au
+		@SerializedName("sortBy") var sortBy: String?, //top
+		@SerializedName("articles") var articles: List<Article>?
 )
 
 data class Article(
-		@SerializedName("author") val author: String?, //http://www.abc.net.au/news/5511636, http://www.abc.net.au/news/lucy-sweeney/7114718
-		@SerializedName("title") val title: String?, //Senate President reignites citizenship debacle, says father may be British
-		@SerializedName("description") val description: String?, //Liberal senator Stephen Parry tells the Government he may be a dual citizen, saying he has asked British authorities to examine his history.
-		@SerializedName("url") val url: String?, //http://www.abc.net.au/news/2017-10-31/stephen-parry-tells-government-his-father-may-be-british/9104482
-		@SerializedName("urlToImage") val urlToImage: String?, //http://www.abc.net.au/news/image/9105122-1x1-700x700.jpg
-		@SerializedName("publishedAt") val publishedAt: String? //2017-10-31T07:02:48Z
+		@SerializedName("author") var author: String?, //http://www.abc.net.au/news/5511636, http://www.abc.net.au/news/lucy-sweeney/7114718
+		@SerializedName("title") var title: String?, //Senate President reignites citizenship debacle, says father may be British
+		@SerializedName("description") var description: String?, //Liberal senator Stephen Parry tells the Government he may be a dual citizen, saying he has asked British authorities to examine his history.
+		@SerializedName("url") var url: String?, //http://www.abc.net.au/news/2017-10-31/stephen-parry-tells-government-his-father-may-be-british/9104482
+		@SerializedName("urlToImage") var urlToImage: String?, //http://www.abc.net.au/news/image/9105122-1x1-700x700.jpg
+		@SerializedName("publishedAt") var publishedAt: String? //2017-10-31T07:02:48Z
 )
