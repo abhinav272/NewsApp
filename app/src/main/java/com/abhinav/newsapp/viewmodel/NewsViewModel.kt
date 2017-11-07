4d68e521cfbb8a53e951da8ab71ba117ac334c49
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-07 23:14:45 +0530

    temp changes to AndroidViewModel

--- app/src/main/java/com/abhinav/newsapp/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/abhinav/newsapp/viewmodel/NewsViewModel.kt
@@ -1,7 +1,12 @@
 package com.abhinav.newsapp.ui.viewmodel
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
+import android.content.Context
+import com.abhinav.newsapp.api.Resource
+import com.abhinav.newsapp.db.SourceEntity
 import com.abhinav.newsapp.ui.api.APIInterface
 import com.abhinav.newsapp.ui.model.ArticlesResponse
 import com.abhinav.newsapp.ui.model.SourceResponse
@@ -10,12 +15,13 @@ import com.abhinav.newsapp.ui.repo.NewsRepository
 /**
  * Created by abhinav.sharma on 01/11/17.
  */
-class NewsViewModel : ViewModel() {
+class NewsViewModel(application: Application) : AndroidViewModel(application) {
 
     private val newsRepo : NewsRepository = NewsRepository(APIInterface.getNewsAPIService())
+    val context: Context = application.applicationContext
 
-    fun getNewsSource(language: String?, category: String?, country: String?): LiveData<SourceResponse>{
-        return newsRepo.getNewsSource(language, category, country)
+    fun getNewsSource(language: String?, category: String?, country: String?): LiveData<Resource<List<SourceEntity>>> {
+        return newsRepo.fetchNewsSource(context,language, category, country)
     }
 
     fun getNewsArticles(source: String, sortBy: String?) : LiveData<ArticlesResponse> {
