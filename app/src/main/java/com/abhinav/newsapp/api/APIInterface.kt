cdf117fb090e9287467144c332666e230cb565d2
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-07 23:10:20 +0530

    adapter added and return type changed

--- app/src/main/java/com/abhinav/newsapp/api/APIInterface.kt
+++ app/src/main/java/com/abhinav/newsapp/api/APIInterface.kt
@@ -1,9 +1,12 @@
 package com.abhinav.newsapp.ui.api
 
+import android.arch.lifecycle.LiveData
+import com.abhinav.newsapp.api.ApiResponse
 import com.abhinav.newsapp.ui.model.ArticlesResponse
 import com.abhinav.newsapp.ui.model.SourceResponse
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -21,6 +24,7 @@ interface APIInterface {
             return Retrofit.Builder()
                     .baseUrl(NEWSAPI_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build()
                     .create(APIInterface::class.java)
         }
@@ -29,7 +33,7 @@ interface APIInterface {
     @GET("sources")
     fun getSources(@Query("language") language: String?,
                    @Query("category") category: String?,
-                   @Query("country") country: String?): Call<SourceResponse>
+                   @Query("country") country: String?): LiveData<ApiResponse<SourceResponse>>
 
     @GET("articles")
     fun getArticles(@Query("source") source: String,
